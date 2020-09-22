5d2881d4eb1436f8e6f03acdb46303e3acbc24e8
Marcos Sevilla <marcossevilla14@gmail.com>
2020-09-22 00:32:05 -0600

    working on chat input widget

--- lib/ui/screens/chat.dart
+++ lib/ui/screens/chat.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -46,12 +49,64 @@ class ChatScreen extends StatelessWidget {
             ),
           ),
           const Divider(height: 1),
-          Container(
-            height: 100,
-            color: Colors.red,
-          ),
+          const _ChatInput(),
         ],
       ),
     );
+  }
+}
+
+class _ChatInput extends StatefulWidget {
+  const _ChatInput({Key key}) : super(key: key);
+
+  @override
+  __ChatInputState createState() => __ChatInputState();
+}
+
+class __ChatInputState extends State<_ChatInput> {
+  final _controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 10),
+          child: Row(
+            children: [
+              Flexible(
+                child: TextField(
+                  controller: _controller,
+                  onSubmitted: (value) {},
+                  onChanged: (value) {
+                    setState(() => _controller.text = value);
+                  },
+                  textInputAction: TextInputAction.send,
+                  decoration: const InputDecoration(
+                    hintText: 'Send message...',
+                  ),
+                ),
+              ),
+              if (Platform.isAndroid)
+                IconButton(
+                  icon: Icon(Icons.send, color: Colors.blue.shade400),
+                  onPressed: _controller.text.isEmpty ? null : _handleSubmit,
+                )
+              else
+                CupertinoButton(
+                  child: const Text('Send'),
+                  onPressed: _controller.text.isEmpty ? null : _handleSubmit,
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleSubmit() {
+    print(_controller.text);
+    setState(_controller.clear);
   }
 }
